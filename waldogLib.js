7856b5d63ec01309ee060af001e8df319a08990d
Walden Wang <walwang@catorpww4003.global.publicisgroupe.net>
2017-06-16 10:12:13 -0400

    added check object function

--- waldogLib.js
+++ waldogLib.js
@@ -1,6 +1,6 @@
 // The following code has all the useful JS functions that Waldog wrote
 
-//get query string from URL 
+//-get query string from URL 
 var url_promo_check = window.location.href;
 function getParameterByName(name, url) {
     if (!url) {
@@ -15,3 +15,23 @@ function getParameterByName(name, url) {
 }
 var nodeID = getParameterByName('promo', url_promo_check);
 //end get query string from URL
+
+//-check object property and return value
+// Setup
+var myObj = {
+  gift: "pony",
+  pet: "kitten",
+  bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+
+  if(myObj.hasOwnProperty(checkProp)){
+    return myObj[checkProp];//the other method to check property is myObj.checkProp but since the testing value has quotes, it can only use square brackets
+  }else{
+    return "Not Found";
+  }
+}
+// Test your code by modifying these values
+checkObj("gift");
+//end check object property and return value
